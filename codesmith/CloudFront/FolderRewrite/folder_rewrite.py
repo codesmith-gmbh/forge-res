3eeba2ef7e64e6bb966381e102db1b1368e6ef5c
Stanislas Nanchen <240862+stanislas@users.noreply.github.com>
2020-01-23 11:16:38 +0100

    comment

--- codesmith/CloudFront/FolderRewrite/folder_rewrite.py
+++ codesmith/CloudFront/FolderRewrite/folder_rewrite.py
@@ -4,6 +4,7 @@ from pathlib import Path
 def handler(event, _):
     request = event['Records'][0]['cf']['request']
     uri = request['uri']
+    # We consider "file" names without dot to be folders
     if '.' not in Path(uri).name:
         prefix = 'index.html' if uri.endswith('/') else '/index.html'
         request['uri'] += prefix
